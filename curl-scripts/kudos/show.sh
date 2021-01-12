239d3d9ecb00f18756d660de50e66add3b6a8b05
jmsgnnr <jmsgnnr@gmail.com>
2021-01-11 21:46:30 -0700

    Made changes to kudos curl scripts

--- curl-scripts/kudos/show.sh
+++ curl-scripts/kudos/show.sh
@@ -1,4 +1,4 @@
-curl 'https://frozen-shore-45480.herokuapp.com/kudos' \
+curl "https://frozen-shore-45480.herokuapp.com/kudos/${ID}" \
   --include \
   --request GET \
   --header "Content-Type: application/json" \
